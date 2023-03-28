69edfc1db125d520b78b93c65bf2ff804b92e4d5
mattiolka <mattiolka@gmail.com>
2023-03-28 22:33:16 +0300

    Додала функцыю стрибка

--- app.js
+++ app.js
@@ -4,11 +4,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function control(e) {
         if ( e.keyCode === 32) {
-            console.log('pressed');
+            jump();
         }
     }
 
     document.addEventListener('keyup', control);
+
+    function jump(){
+        let position = 0;
+        let timerId = setInterval(() => {
+
+            // move down
+            if (position === 150) {
+                clearInterval(timerId)
+                console.log('down');
+                let downTimerId = setInterval(() => {
+                    if(position === 0) {
+                        clearInterval(downTimerId);
+                    }
+                    position -= 30;
+                    dino.style.bottom = position + 'px';   
+                }, 20)
+            }
+
+
+
+            // move up
+            console.log('up');
+            position += 30;
+            dino.style.bottom = position + 'px';  
+        }, 20)
+    }
+
 
 
 })
