068fd6b9e7451338f2c0f340ac283af456433ea3
mattiolka <mattiolka@gmail.com>
2023-03-29 08:33:56 +0300

    Додала гравітацію для стрибка

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dino = document.querySelector('.dino');
     let isJamping = false;
+    let gravity = 0.9;
 
 
     function control(e) {
@@ -14,28 +15,36 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.addEventListener('keyup', control);
 
+    let position = 0;
+
     function jump(){
+        let count = 0;
         let position = 0;
         let timerId = setInterval(() => {
 
             // move down
-            if (position === 150) {
+            if (count === 15) {
                 clearInterval(timerId)
                 console.log('down');
                 let downTimerId = setInterval(() => {
-                    if(position === 0) {
+                    if(count === 0) {
                         clearInterval(downTimerId);
                         isJamping = false;
                     }
-                    position -= 30;
+                    position -= 5;
+                    count --;
+                    position = position * gravity;
                     dino.style.bottom = position + 'px';   
                 }, 20)
             }
 
             // move up
             console.log('up');
+            count ++;
             position += 30;
-            dino.style.bottom = position + 'px';  
+            position = position * gravity;
+            dino.style.bottom = position + 'px';
+            console.log(dino.style.bottom);  
         }, 20)
     }
 
