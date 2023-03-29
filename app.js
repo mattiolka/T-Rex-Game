3114bdbef2889da9b78c9ab606358c2730ce615a
mattiolka <mattiolka@gmail.com>
2023-03-29 18:16:09 +0300

    Додала рух перешкод та повідомлення програшу

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dino = document.querySelector('.dino');
-    const grid = document.querySelector('.grid');
+    const grid = document.querySelector('.grid'); 
+    const alertText = document.getElementById('alert')
     let isJamping = false;
     let gravity = 0.9;
-
+    let isGameOver = false;
 
     function control(e) {
         if ( e.keyCode === 32) {
@@ -13,11 +14,10 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
-
     document.addEventListener('keyup', control);
 
     let position = 0;
-
+ 
     function jump(){
         let count = 0;
         let position = 0;
@@ -50,14 +50,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function generateObstacles() {
+        let randomTime = Math.random() * 4000;
         let obstaclePosition = 1000;
         const obstacle = document.createElement('div');
         obstacle.classList.add('obstacle');
         grid.appendChild(obstacle);
         obstacle.style.left = obstaclePosition + 'px';
+
+        let timerId = setInterval(() => {
+            if(obstaclePosition > 0 && obstaclePosition < 60 && position < 60){
+                clearInterval(timerId);
+                alertText.innerHTML = 'Кінець гри!';
+                isGameOver = true;
+            }
+
+            obstaclePosition -= 10;
+            obstacle.style.left = obstaclePosition + 'px';  
+        }, 20)
+
+        if(!isGameOver) setTimeout(generateObstacles, randomTime);
     }
     generateObstacles();  
-
-
-
 })
